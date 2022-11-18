e886b25f6570c7f1963848ec62950949a8a310df
Svetlana <svetlanatat@mail.ru>
2022-11-18 16:14:10 +0300

    Домашнее задание 4-6 к занятию 11 решение 2

---
+++ assets/scripts/scripts_занятие 11_задание 4-6_2.js
@@ -0,0 +1,12 @@
+let value1 = 3;
+let value2 = 9;
+
+let result = min(value1, value2);
+function max(value1, value2) {
+    if (value1 > value2) {
+      return value1;
+    } else {
+      return value2;
+    }
+  }
+console.log (result);
