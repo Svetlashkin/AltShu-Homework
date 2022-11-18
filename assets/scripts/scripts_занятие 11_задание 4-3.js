6bfc08aeb151c83a77fb400338708a46b6fc39d6
Svetlana <svetlanatat@mail.ru>
2022-11-18 14:05:47 +0300

    Внесены измения в код js в задание 4-3

--- assets/scripts/scripts_занятие 11_задание 4-3.js
+++ assets/scripts/scripts_занятие 11_задание 4-3.js
@@ -1,9 +1,11 @@
 let value = +prompt("Enter a number");
 
-if (value > 0 ) {
+if (isNaN(value)) {
+    alert ("Error");
+} else if (value > 0 ) {
     alert (1);
 } else if (value < 0 ) {
     alert (-1);
-} else if (value == 0 ) {
+} else {
     alert (0);
 }
