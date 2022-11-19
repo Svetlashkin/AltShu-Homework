3af0a4a9127fb7d65e5bb81be299f22db7a9f55e
Svetlana <svetlanatat@mail.ru>
2022-11-20 00:29:31 +0300

    Задание 4-9 к занятию 11

---
+++ assets/scripts/scripts_занятие 11_задание 4-9_.js
@@ -0,0 +1,8 @@
+let login = prompt ('Введите Ваш логин');
+let pass = prompt ('Введите Ваш пароль');
+
+    if (login == 'admin' && pass == 'password') {
+      alert ('Добро пожаловать, создатель');
+    } else {
+      alert ('Ты не пройдешь!');
+    }
