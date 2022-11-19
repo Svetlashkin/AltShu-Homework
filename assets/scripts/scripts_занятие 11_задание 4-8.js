d584ba98efb404478dcadf4f0aa0fe9c68b915ae
Svetlana <svetlanatat@mail.ru>
2022-11-19 23:12:41 +0300

    Домашнее задание 4-8 к занятию 11

---
+++ assets/scripts/scripts_занятие 11_задание 4-8.js
@@ -0,0 +1,41 @@
+let month = +prompt('Введите месяц в виде числа?');
+switch (month) {
+  case 1:
+    alert( 'Январь' );
+    break;
+  case 2:
+    alert( 'Февраль' );
+    break;
+  case 3:
+    alert( 'Март' );
+    break;
+  case 4:
+    alert( 'Апрель' );
+    break;
+  case 5:
+    alert( 'Май' );
+    break; 
+  case 6:
+    alert( 'Июнь' );
+    break; 
+  case 7:
+    alert( 'Июль' );
+    break;
+  case 8:
+    alert( 'Август' );
+    break;
+  case 9:
+    alert( 'Сентябрь' );
+    break;
+  case 10:
+    alert( 'Октябрь' );
+    break;
+  case 11:
+    alert( 'Ноябрь' );
+    break;
+  case 12:
+    alert( 'Декабрь' );
+    break;
+  default:
+    alert( 'Неизвестное значение' );
+}
