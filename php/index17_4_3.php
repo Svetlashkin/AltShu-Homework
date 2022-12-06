714d5d4e55771b1a455fa86dcde795f5168962f2
Svetlana <svetlanatat@mail.ru>
2022-12-06 13:54:25 +0300

    Домашнее задание 4-3 к занятию 17

---
+++ php/index17_4_3.php
@@ -0,0 +1,21 @@
+<?php
+$a = 5 + 2;
+$b = 5 - 2;
+$c = 5 * 2;
+$d = 5 / 2;
+$e = 5 % 2;
+$f = 5 ** 2;
+
+
+echo ($a);
+echo ('</br>');
+echo ($b);
+echo ('</br>');
+echo ($c);
+echo ('</br>');
+echo ($d);
+echo ('</br>');
+echo ($e);
+echo ('</br>');
+echo ($f);
+?>
